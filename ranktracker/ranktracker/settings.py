9c64ce0922e313178b16f7443da12de27e920f71
rthorslev <r.thorslev@gmail.com>
2025-09-23 14:04:18 +0200

    Added django-cors-headers to requirements and middleware in settings. Set CORS_ALLOW_ALL_ORIGINS = True for development purposes '

--- ranktracker/ranktracker/settings.py
+++ ranktracker/ranktracker/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'django_filters',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
